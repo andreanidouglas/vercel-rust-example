6e2a7406e325520afe35976dd8f383fa0c893f7a
Douglas A <andreani.dr@gmail.com>
2023-04-05 19:28:03 -0300

    initial development

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,15 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn xor(a: u32, b: u32) -> u32 {
-    return a ^ b;
+pub fn xor(a: u32, b: u32) -> Result<u32, JsValue> {
+    return Ok(a ^ b);
+}
+
+#[wasm_bindgen]
+pub fn get(i: usize) -> Result<usize, JsError> {
+    match i {
+        69 => Ok(i),
+        _ => Err(JsError::new("Could not get number"))
+    }
+    
 }
